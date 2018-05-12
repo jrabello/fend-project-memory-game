587b274e024768dc8b8080927041af24606d90fd
Joilson Rabelo <joilson.rabello@gmail.com>
2018-05-12 19:29:34 -0300

    Create cards structure descriptors

--- js/app.ts
+++ js/app.ts
@@ -1,7 +1,24 @@
+// I coded everything using typescript, 
+// quite similar to js and I'm sure you guys will understand :D
+interface ICardDescriptor {
+    uid: string;
+    uidFkPair: string;
+    visible: boolean;
+    matched: boolean;
+}
+
+interface ICardMap {
+    [uid: string]: ICardDescriptor;
+}
+
+interface ITableDescriptor {
+
+}
+
 /*
  * Create a list that holds all of your cards
  */
-let cards = [
+let cards: string[]  = [
     'fa-diamond',
     'fa-anchor',
     'fa-bomb',
@@ -10,40 +27,55 @@ let cards = [
     'fa-bicycle',
     'fa-paper-plane-o',
     'fa-cube',
-];
-const defaultCardDescriptor = {
+]
+const defaultCardDescriptor: ICardDescriptor = {
     uid: '',
     uidFkPair: '',
     visible: false,
     matched: false,
-};
-const cardMap = {};
+}
+const cardMap: ICardMap = {}
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-function onInit() {
+function onInit(): void {
     // build card map
-    cards.forEach((card) => {
+    cards.forEach((card: string) => {
+
         // this trick allows me to check another card visibility in O(1)
         // by storing a reference to it's pair we can check it fast using a hashmap :)
         cards.push(card);
-        const cardFkUid = card + ((this.cards.length + 1).toString());
-        cardMap[card] = Object.assign({}, defaultCardDescriptor, { uid: card, uidFkPair: cardFkUid });
-        cardMap[cardFkUid] = Object.assign({}, defaultCardDescriptor, { uid: cardFkUid, uidFkPair: card });
-    });
+        const cardFkUid = card+((this.cards.length+1).toString());
+        
+        cardMap[card] = {
+            ...defaultCardDescriptor,
+            uid: card,
+            uidFkPair: cardFkUid,
+        };
+        cardMap[cardFkUid] = {
+            ...defaultCardDescriptor,
+            uid: cardFkUid,
+            uidFkPair: card,
+        }
+    })
+
     // once we built the cardMap we can start the game
     startGame();
 }
+
 function startGame() {
     cards = shuffle(cards);
     console.log(cards);
 }
+
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
+function shuffle(array: string[]): string[] {
     var currentIndex = array.length, temporaryValue, randomIndex;
+
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -51,8 +83,10 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
+
     return array;
 }
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -63,6 +97,6 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function(event) {
     onInit();
 });
