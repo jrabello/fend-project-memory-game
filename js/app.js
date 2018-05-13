80fa97d30879d6b52e0e43e3393fc391226cfdc1
Joilson Rabelo <joilson.rabello@gmail.com>
2018-05-13 00:49:00 -0300

    Removes ul from DOM 	- created new algorithm for ul insertion in  DOM

--- js/app.js
+++ js/app.js
@@ -27,17 +27,26 @@ var defaultCardDescriptor = {
     matched: false
 };
 var boardDescriptor = {
+    cardMap: {},
     matchedCards: 0,
-    cardMap: {}
+    numberOfCards: cards.length * 2,
+    waitingAnimationFinish: false
 };
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+/**
+ * Called when DOM is loaded
+ * It's responsible for building necessary data structures and then start the game
  */
 function onInit() {
-    // build card map
+    // building card map
+    buildCardMap();
+    // once we built the cardMap we can start the game
+    onStartGame();
+}
+/**
+ * Builds card map to allow faster card pair lookup
+ */
+function buildCardMap() {
+    // builds card map
     cards.forEach(function (card) {
         // this trick allows me to check another card visibility in O(1)
         // by storing a reference to it's pair we can check it fast using a hashmap :)
@@ -46,34 +55,85 @@ function onInit() {
         boardDescriptor.cardMap[card] = __assign({}, defaultCardDescriptor, { uid: card, uidFkPair: cardFkUid, "class": card });
         boardDescriptor.cardMap[cardFkUid] = __assign({}, defaultCardDescriptor, { uid: cardFkUid, uidFkPair: card, "class": card });
     });
-    // once we built the cardMap we can start the game
-    onStartGame();
 }
 /**
- * Starts game
+ * Handles game start init
  */
 function onStartGame() {
     console.log('startGame: ', boardDescriptor.cardMap);
-    // shuffling cards
-    cards = shuffle(cards);
-    // removing cards from deck if any
+    var start = performance.now();
+    // shuffling cards both O(n)
+    var cardKeys = Object.keys(boardDescriptor.cardMap);
+    var cardKeysShuffled = shuffle(cardKeys);
+    // removing deck if any
     var deck = document.querySelector("#deck");
-    while (deck.firstChild) {
-        deck.removeChild(deck.firstChild);
+    if (!!deck) {
+        deck.removeEventListener('click', onDeckClicked);
+        deck.remove();
     }
-    // put elements into DOM
+    // build deck
     var fragment = document.createDocumentFragment();
-    for (var _i = 0, cards_1 = cards; _i < cards_1.length; _i++) {
-        var card = cards_1[_i];
+    var ul = document.createElement('ul');
+    ul.id = ul.className = 'deck';
+    ul.addEventListener("click", onDeckClicked);
+    fragment.appendChild(ul);
+    // building cards
+    for (var _i = 0, cardKeysShuffled_1 = cardKeysShuffled; _i < cardKeysShuffled_1.length; _i++) {
+        var cardKey = cardKeysShuffled_1[_i];
         var li = document.createElement('li');
         var i = document.createElement('i');
         li.className = "card";
-        i.className = "fa " + card;
+        li.id = boardDescriptor.cardMap[cardKey].uid;
+        i.className = "fa " + boardDescriptor.cardMap[cardKey]["class"];
         li.appendChild(i);
-        fragment.appendChild(li);
+        ul.appendChild(li);
     }
-    deck.appendChild(fragment);
+    // inserting deck into DOM
+    document.querySelector('#container').appendChild(fragment);
+    console.log(performance.now() - start);
 }
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+// Listens for clicks on deck
+// and handles clicks on cards via event delegation(to avoid lots of handlers)
+function onDeckClicked(event) {
+    console.log('deck clicked: ', event);
+    // ignoring click event in some cases
+    // when user clicks on deck itself
+    var clickedHtmlElement = event.target;
+    var nodeName = clickedHtmlElement.nodeName.toLowerCase();
+    if (nodeName === 'ul') {
+        return;
+    }
+    // if user clicked into i we need to get its parent(li)
+    if (nodeName === 'i') {
+        clickedHtmlElement = clickedHtmlElement.parentElement;
+    }
+    // here we have li, so we can get it's id since it's unique
+    var currentCard = boardDescriptor.cardMap[clickedHtmlElement.id];
+    // ignoring some click events if matches some constraints 
+    if (currentCard.matched) {
+        return;
+    }
+    // changing card state
+    currentCard.visible = !currentCard.visible;
+    clickedHtmlElement.classList.toggle('open');
+    clickedHtmlElement.classList.toggle('show');
+}
+// Called when DOM is parsed and ready to be modified
+document.addEventListener("DOMContentLoaded", function (event) {
+    onInit();
+});
+// Helper functions
+// Fisher-Yates (aka Knuth) Shuffle
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -86,19 +146,3 @@ function shuffle(array) {
     }
     return array;
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-document.addEventListener("DOMContentLoaded", function (event) {
-    onInit();
-});
-document.querySelector("#restart").addEventListener("click", function (event) {
-    onStartGame();
-});
--- js/app.ts
+++ js/app.ts
@@ -2,7 +2,6 @@
  * I coded everything using typescript, 
  * strong typing ftw :D
  */
-type TICardDescriptorList = ICardDescriptor[];
 interface ICardDescriptor {
     uid: string;
     uidFkPair: string;
@@ -16,14 +15,16 @@ interface ICardMap {
 }
 
 interface IBoardDescriptor {
-    matchedCards: number;
     cardMap: ICardMap;
+    matchedCards: number;
+    numberOfCards: number;
+    waitingAnimationFinish: boolean;
 }
 
 /*
  * Create a list that holds all of your cards
  */
-let cards: string[]  = [
+let cards: string[] = [
     'fa-diamond',
     'fa-anchor',
     'fa-bomb',
@@ -41,25 +42,37 @@ const defaultCardDescriptor: ICardDescriptor = {
     matched: false,
 }
 const boardDescriptor: IBoardDescriptor = <IBoardDescriptor>{
+    cardMap: {},
     matchedCards: 0,
-    cardMap: {}
+    numberOfCards: cards.length * 2,
+    waitingAnimationFinish: false,
 }
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+
+/**
+ * Called when DOM is loaded
+ * It's responsible for building necessary data structures and then start the game
  */
 function onInit(): void {
-    // build card map
+    // building card map
+    buildCardMap();
+
+    // once we built the cardMap we can start the game
+    onStartGame();
+}
+
+/**
+ * Builds card map to allow faster card pair lookup
+ */
+function buildCardMap(): void {
+    // builds card map
     cards.forEach((card: string) => {
 
         // this trick allows me to check another card visibility in O(1)
         // by storing a reference to it's pair we can check it fast using a hashmap :)
         cards.push(card);
-        const cardFkUid = card+((cards.length).toString());
-        
+        const cardFkUid = card + ((cards.length).toString());
+
         boardDescriptor.cardMap[card] = {
             ...defaultCardDescriptor,
             uid: card,
@@ -73,38 +86,101 @@ function onInit(): void {
             class: card,
         }
     })
-
-    // once we built the cardMap we can start the game
-    onStartGame();
 }
 
 /**
- * Starts game
+ * Handles game start init
  */
-function onStartGame() {
+function onStartGame(): void {
     console.log('startGame: ', boardDescriptor.cardMap);
-    // shuffling cards
-    cards = shuffle(cards);
-    
-    // removing cards from deck if any
+    const start = performance.now();
+
+    // shuffling cards both O(n)
+    const cardKeys = Object.keys(boardDescriptor.cardMap);
+    const cardKeysShuffled = shuffle(cardKeys);
+
+    // removing deck if any
     const deck = document.querySelector("#deck")
-    while(deck.firstChild){
-        deck.removeChild(deck.firstChild)
+    if (!!deck) {
+        deck.removeEventListener('click', onDeckClicked)
+        deck.remove();
     }
 
-    // put elements into DOM
+    // build deck
     const fragment = document.createDocumentFragment();
-    for (const card of cards) {
+    const ul = document.createElement('ul');
+    ul.id = ul.className = 'deck';
+    ul.addEventListener("click", onDeckClicked);
+    fragment.appendChild(ul);
+
+    // building cards
+    for (const cardKey of cardKeysShuffled) {
         const li = document.createElement('li');
         const i = document.createElement('i');
+
         li.className = `card`;
-        i.className = `fa ${card}`
+        li.id = boardDescriptor.cardMap[cardKey].uid;
+        i.className = `fa ${boardDescriptor.cardMap[cardKey].class}`
+
         li.appendChild(i);
-        fragment.appendChild(li);
+        ul.appendChild(li);
     }
-    deck.appendChild(fragment);
+
+    // inserting deck into DOM
+    document.querySelector('#container').appendChild(fragment);
+    console.log(performance.now() - start);
 }
 
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+// Listens for clicks on deck
+// and handles clicks on cards via event delegation(to avoid lots of handlers)
+function onDeckClicked(event: MouseEvent): void {
+    console.log('deck clicked: ', event);
+    
+    // ignoring click event in some cases
+    // when user clicks on deck itself
+    let clickedHtmlElement: HTMLElement = (<HTMLElement>event.target)
+    const nodeName: string = clickedHtmlElement.nodeName.toLowerCase();
+    if (nodeName === 'ul') {
+        return;
+    }
+    
+    // if user clicked into i we need to get its parent(li)
+    if (nodeName === 'i') {
+        clickedHtmlElement = clickedHtmlElement.parentElement;
+    }
+    
+    // here we have li, so we can get it's id since it's unique
+    let currentCard: ICardDescriptor = boardDescriptor.cardMap[clickedHtmlElement.id];
+    
+    // ignoring some click events if matches some constraints 
+    if (currentCard.matched) {
+        return;
+    }
+    
+    // changing card state
+    currentCard.visible = !currentCard.visible;
+    clickedHtmlElement.classList.toggle('open');
+    clickedHtmlElement.classList.toggle('show');
+}
+
+// Called when DOM is parsed and ready to be modified
+document.addEventListener("DOMContentLoaded", (event) => {
+    onInit();
+});
+
+// Helper functions
+// Fisher-Yates (aka Knuth) Shuffle
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array: string[]): string[] {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -119,21 +195,3 @@ function shuffle(array: string[]): string[] {
 
     return array;
 }
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-document.addEventListener("DOMContentLoaded", function(event) {
-    onInit();
-});
-
-document.querySelector("#restart").addEventListener("click", function( event ) {
-    onStartGame();
-});
